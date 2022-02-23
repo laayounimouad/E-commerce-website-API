1b9f13c3f0f9e2b757f145fc2141319338f29d30
mouad01laayouni <mouad01laayouni@gmail.com>
2022-02-23 14:50:00 +0100

    Update product.js

--- models/product.js
+++ models/product.js
@@ -54,7 +54,7 @@ const productSchema = new mongoose.Schema(
     },
     brand: {
       type: String,
-      enum: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
+      enum: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS", "HP"],
     },
     // ratings: [
     //   {
